4c82262ad37af62ac7b3a9036e14900aef44f66c
shirish17 <shirish.wanjari@gmail.com>
2024-06-14 16:33:27 +0800

    updated project setup

--- src/main/java/pages/LandingPage.java
+++ src/main/java/pages/LandingPage.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-import utilis.Constants;
 import utilis.ElementUtilis;
 
 public class LandingPage {
@@ -49,13 +48,6 @@ public class LandingPage {
 		public void openApplication(String URL) {
 			driver.get(URL);
 		}
-		
-		
-		
-		
-		
-		
-
 	}
 
 
